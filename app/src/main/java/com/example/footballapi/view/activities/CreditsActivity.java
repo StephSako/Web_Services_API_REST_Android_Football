2cf6cedbe1d9cfcff617afb436146baf11593fae
Steph Sako <stephen.sakovitch@orange.fr>
2019-02-07 22:58:30 +0100

    Remplissage du "A propos"

--- app/src/main/java/com/example/footballapi/view/activities/CreditsActivity.java
+++ app/src/main/java/com/example/footballapi/view/activities/CreditsActivity.java
@@ -3,6 +3,7 @@ package com.example.footballapi.view.activities;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.example.footballapi.R;
 
@@ -13,5 +14,8 @@ public class CreditsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.credits);
+
+        TextView tvCredits = findViewById(R.id.tvCredits);
+        tvCredits.setText(getString(R.string.DescriptionAPropos));
     }
 }
